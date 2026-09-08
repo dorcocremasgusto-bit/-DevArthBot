19a92d70fff4968d2b952672ffea917379227e6b
dorcocremasgusto-bit <dorcocremasgusto@gmail.com>
2026-09-08 19:36:20 -0400

    Refactor message handling logic and clean up code

--- utils/events/messageHandler.js
+++ utils/events/messageHandler.js
@@ -3,7 +3,6 @@ import fs from 'fs/promises'
 import group from '../commands/group.js'
 import block from '../commands/block.js'
 import viewonce from '../commands/viewonce.js'
-//import kill from '../commands/kill.js'
 import tiktok from '../commands/tiktok.js'
 import play from '../commands/play.js'
 import sudo from '../commands/sudo.js'
@@ -29,309 +28,266 @@ import { pingTest } from "../commands/ping.js"
 import auto from '../commands/auto.js'
 import uptime from '../commands/uptime.js'
 
+
 async function handleIncomingMessage(client, event) {
-    let lid = client?.user?.lid.split(':')[0] + '@lid'
-    const number = client.user.id.split(':')[0]
-    const messages = event.messages
-    const publicMode = configmanager.config.users[number].publicMode
-    const prefix = configmanager.config.users[number].prefix
 
-    for (const message of messages) {
-        const messageBody = (message.message?.extendedTextMessage?.text ||
-                           message.message?.conversation || '').toLowerCase()
-        const remoteJid = message.key.remoteJid
-        const approvedUsers = configmanager.config.users[number].sudoList
+    try {
 
-        if (!messageBody || !remoteJid) continue
+        const number = client.user.id.split(':')[0]
 
-        console.log('📨 Message:', messageBody.substring(0, 50))
-        
-        auto.autotype(client, message)
-        auto.autorecord(client, message)
-        tag.respond(client, message)
-
-        reactions.auto(
-            client,
-            message,
-            configmanager.config.users[number].autoreact,
-            configmanager.config.users[number].emoji
-        )
-
-        if (messageBody.startsWith(prefix) &&
-            (publicMode ||
-             message.key.fromMe ||
-             approvedUsers.includes(message.key.participant || message.key.remoteJid) ||
-             lid.includes(message.key.participant || message.key.remoteJid))) {
-
-            const commandAndArgs = messageBody.slice(prefix.length).trim()
-            const parts = commandAndArgs.split(/\s+/)
-            const command = parts[0]
-
-            switch (command) {
-                case 'uptime': // @cat: utils
-                    await react(client, message)
-                    await uptime(client, message)
-                    break
-
-                case 'ping': // @cat: utils
-                    await react(client, message)
-                    await pingTest(client, message)
-                    break
-
-                case 'menu': // @cat: utils
-                    await react(client, message)
-                    await info(client, message)
-                    break
-
-                case 'fancy': // @cat: utils
-                    await react(client, message)
-                    await fancy(client, message)
-                    break
-
-                case 'setpp': // @cat: utils
-                    await react(client, message)
-                    await pp.setpp(client, message)
-                    break
-
-                case 'getpp': // @cat: utils
-                    await react(client, message)
-                    await pp.getpp(client, message)
-                    break
-
-                case 'sudo': // @cat: owner
-                    await react(client, message)
-                    await sudo.sudo(client, message, approvedUsers)
-                    configmanager.save()
-                    break
-
-                case 'delsudo': // @cat: owner
-                    await react(client, message)
-                    await sudo.delsudo(client, message, approvedUsers)
-                    configmanager.save()
-                    break
-
-                case 'public': // @cat: settings
-                    await react(client, message)
-                    await set.isPublic(message, client)
-                    break
-
-                case 'setprefix': // @cat: settings
-                    await react(client, message)
-                    await set.setprefix(message, client)
-                    break
-
-                case 'autotype': // @cat: settings
-                    await react(client, message)
-                    await set.setautotype(message, client)
-                    break
-
-                case 'autorecord': // @cat: settings
-                    await react(client, message)
-                    await set.setautorecord(message, client)
-                    break
-
-                case 'welcome': // @cat: settings
-                    await react(client, message)
-                    await set.setwelcome(message, client)
-                    break
-
-                case 'photo': // @cat: media
-                    await react(client, message)
-                    await media.photo(client, message)
-                    break
-
-                case 'toaudio': // @cat: media
-                    await react(client, message)
-                    await media.tomp3(client, message)
-                    break
-
-                case 'sticker': // @cat: media
-                    await react(client, message)
-                    await sticker(client, message)
-                    break
-
-                case 'play': // @cat: media
-                    await react(client, message)
-                    await play(message, client)
-                    break
-
-                case 'img': // @cat: media
-                    await react(client, message)
-                    await img(message, client)
-                    break
-
-                case 'vv': // @cat: media
-                    await react(client, message)
-                    await viewonce(client, message)
-                    break
-
-                case 'save': // @cat: media
-                    await react(client, message)
-                    await save(client, message)
-                    break
-
-                case 'tiktok': // @cat: media
-                    await react(client, message)
-                    await tiktok(client, message)
-                    break
-
-                case 'url': // @cat: media
-                    await react(client, message)
-                    await url(client, message)
-                    break
-
-                case 'tag': // @cat: group
-                    await react(client, message)
-                    await tag.tag(client, message)
-                    break
-
-                case 'tagall': // @cat: group
-                    await react(client, message)
-                    await tag.tagall(client, message)
-                    break
-
-                case 'tagadmin': // @cat: group
-                    await react(client, message)
-                    await tag.tagadmin(client, message)
-                    break
-
-                case 'kick': // @cat: group
-                    await react(client, message)
-                    await group.kick(client, message)
-                    break
-
-                case 'kickall': // @cat: group
-                    await react(client, message)
-                    await group.kickall(client, message)
-                    break
-
-                case 'kickall2': // @cat: group
-                    await react(client, message)
-                    await group.kickall2(client, message)
-                    break
-
-                case 'promote': // @cat: group
-                    await react(client, message)
-                    await group.promote(client, message)
-                    break
-
-                case 'demote': // @cat: group
-                    await react(client, message)
-                    await group.demote(client, message)
-                    break
-
-                case 'promoteall': // @cat: group
-                    await react(client, message)
-                    await group.pall(client, message)
-                    break
-
-                case 'demoteall': // @cat: group
-                    await react(client, message)
-                    await group.dall(client, message)
-                    break
-
-                case 'mute': // @cat: group
-                    await react(client, message)
-                    await group.mute(client, message)
-                    break
-
-                case 'unmute': // @cat: group
-                    await react(client, message)
-                    await group.unmute(client, message)
-                    break
-
-                case 'gclink': // @cat: group
-                    await react(client, message)
-                    await group.gclink(client, message)
-                    break
-
-                case 'antilink': // @cat: group
-                    await react(client, message)
-                    await group.antilink(client, message)
-                    break
-
-                case 'bye': // @cat: group
-                    await react(client, message)
-                    await group.bye(client, message)
-                    break
-
-                case 'block': // @cat: moderation
-                    await react(client, message)
-                    await block.block(client, message)
-                    break
-
-                case 'unblock': // @cat: moderation
-                    await react(client, message)
-                    await block.unblock(client, message)
-                    break
-
-                case 'close': // @cat: bug
-                    await react(client, message)
-                    await hell(client, message)
-                    break
-
-               // case 'kill': // @cat: bug
-                  //  await react(client, message)
-                  //  await kill(client, message)
-                  //  break
-
-                case 'fuck': // @cat: bug
-                    await react(client, message)
-                    await fuck(client, message)
-                    break
-
-                case 'addprem': // @cat: premium
-    await react(client, message);
-    await premiums.addprem(client, message);
-    configmanager.saveP();
-    break;
-
-case 'delprem': // @cat: premium
-    await react(client, message);
-    await premiums.delprem(client, message);
-    configmanager.saveP();
-    break;
-
-                case 'test': // @cat: creator
-                    await react(client, message)
-                    break
-
-                case 'join': // @cat: group
-                    await react(client, message)
-                    await group.setJoin(client, message)
-                    break
-
-                case 'auto-promote': // @cat: premium
-                    await react(client, message)
-                    if (premium.includes(number + "@s.whatsapp.net")) {
-                        await group.autoPromote(client, message)
-                    } else {
-                        await bug(client, message, "command only for premium users.", 3)
-                    }
-                    break
-
-                case 'auto-demote': // @cat: premium
-                    await react(client, message)
-                    if (premium.includes(number + "@s.whatsapp.net")) {
-                        await group.autoDemote(client, message)
-                    } else {
-                        await bug(client, message, "command only for premium users.", 3)
-                    }
-                    break
-
-                case 'auto-left': // @cat: premium
-                    await react(client, message)
-                    if (premium.includes(number + "@s.whatsapp.net")) {
-                        await group.autoLeft(client, message)
-                    } else {
-                        await bug(client, message, "command only for premium users.", 3)
-                    }
-                    break
+        if (!configmanager.config.users[number]) {
+            configmanager.config.users[number] = {
+                sudoList: [
+                    `${number}@s.whatsapp.net`
+                ],
+                prefix: '.',
+                publicMode: true,
+                autoreact: false,
+                emoji: '🎯'
             }
+
+            configmanager.save()
         }
 
-        await group.linkDetection(client, message)
+
+        const lid = client?.user?.lid
+            ? client.user.lid.split(':')[0] + '@lid'
+            : ''
+
+
+        const messages = event.messages
+
+        const userConfig = configmanager.config.users[number]
+
+        const publicMode = userConfig.publicMode
+        const prefix = userConfig.prefix
+
+        for (const message of messages) {
+
+            const messageBody =
+                (
+                    message.message?.extendedTextMessage?.text ||
+                    message.message?.conversation ||
+                    ''
+                ).toLowerCase()
+
+
+            const remoteJid = message.key.remoteJid
+
+
+            const approvedUsers = userConfig.sudoList || []
+
+
+            if (!messageBody || !remoteJid) continue
+
+
+            console.log(
+                '📨 Message:',
+                messageBody.substring(0,50)
+            )
+
+
+            auto.autotype(client, message)
+            auto.autorecord(client, message)
+
+            await tag.respond(client, message)
+
+
+            reactions.auto(
+                client,
+                message,
+                userConfig.autoreact,
+                userConfig.emoji
+            )
+
+
+
+            if (
+                messageBody.startsWith(prefix) &&
+                (
+                    publicMode ||
+                    message.key.fromMe ||
+                    approvedUsers.includes(
+                        message.key.participant ||
+                        message.key.remoteJid
+                    ) ||
+                    lid.includes(
+                        message.key.participant ||
+                        message.key.remoteJid
+                    )
+                )
+            ) {
+
+
+                const args =
+                    messageBody
+                    .slice(prefix.length)
+                    .trim()
+                    .split(/\s+/)
+
+
+                const command = args[0]
+
+
+
+                switch(command) {
+
+
+                    case 'uptime':
+                        await react(client,message)
+                        await uptime(client,message)
+                        break
+
+
+                    case 'ping':
+                        await react(client,message)
+                        await pingTest(client,message)
+                        break
+
+
+                    case 'menu':
+                        await react(client,message)
+                        await info(client,message)
+                        break
+
+
+                    case 'sticker':
+                        await react(client,message)
+                        await sticker(client,message)
+                        break
+
+
+                    case 'play':
+                        await react(client,message)
+                        await play(message,client)
+                        break
+
+
+                    case 'img':
+                        await react(client,message)
+                        await img(message,client)
+                        break
+
+
+                    case 'tiktok':
+                        await react(client,message)
+                        await tiktok(client,message)
+                        break
+
+
+                    case 'url':
+                        await react(client,message)
+                        await url(client,message)
+                        break
+
+
+                    case 'save':
+                        await react(client,message)
+                        await save(client,message)
+                        break
+
+
+                    case 'sudo':
+                        await react(client,message)
+                        await sudo.sudo(client,message,approvedUsers)
+                        configmanager.save()
+                        break
+
+
+                    case 'delsudo':
+                        await react(client,message)
+                        await sudo.delsudo(client,message,approvedUsers)
+                        configmanager.save()
+                        break
+
+
+                    case 'public':
+                        await react(client,message)
+                        await set.isPublic(message,client)
+                        break
+
+
+                    case 'setprefix':
+                        await react(client,message)
+                        await set.setprefix(message,client)
+                        break
+
+
+                    case 'kick':
+                        await react(client,message)
+                        await group.kick(client,message)
+                        break
+
+
+                    case 'promote':
+                        await react(client,message)
+                        await group.promote(client,message)
+                        break
+
+
+                    case 'demote':
+                        await react(client,message)
+                        await group.demote(client,message)
+                        break
+
+
+                    case 'tagall':
+                        await react(client,message)
+                        await tag.tagall(client,message)
+                        break
+
+
+                    case 'block':
+                        await react(client,message)
+                        await block.block(client,message)
+                        break
+
+
+                    case 'unblock':
+                        await react(client,message)
+                        await block.unblock(client,message)
+                        break
+
+
+                    case 'fuck':
+                        await react(client,message)
+                        await fuck(client,message)
+                        break
+
+
+                    case 'addprem':
+                        await react(client,message)
+                        await premiums.addprem(client,message)
+                        configmanager.saveP()
+                        break
+
+
+                    case 'delprem':
+                        await react(client,message)
+                        await premiums.delprem(client,message)
+                        configmanager.saveP()
+                        break
+
+                }
+
+            }
+
+
+            await group.linkDetection(client,message)
+
+        }
+
+
+    } catch(err) {
+
+        console.error(
+            "❌ Message handler error:",
+            err
+        )
+
     }
+
 }
+
 
 export default handleIncomingMessage
